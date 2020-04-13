ee557487768adfb2314b4307e3b870110cfcf2d6
Alymbekov <maxim.makarov.1997@mail.ru>
2020-04-13 20:51:12 +0600

    add smtp server

--- limon_project/settings.py
+++ limon_project/settings.py
@@ -122,4 +122,9 @@ STATIC_URL = '/static/'
 
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'alymbekovdastan1@@gmail.com'
+EMAIL_HOST_PASSWORD = 'vamxmoebnxnufauh'
